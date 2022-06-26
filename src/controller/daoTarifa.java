b0a84e7e3a660371906562b27a6a675a34b80bfe
PedroNuernberg <80931128+PedroNuernberg@users.noreply.github.com>
2022-06-25 21:08:00 -0300

    Create daoTarifa.java
    criar dao Tarifa

---
+++ src/controller/daoTarifa.java
@@ -0,0 +1,23 @@
+
+package controller;
+
+import abstratas.dao;
+import java.util.List;
+import model.PontoTuristico;
+import model.Tarifa;
+
+public class daoTarifa extends dao<Tarifa> {
+    public List<Tarifa> read(){
+        String JPQL="select t from Tarifa t order by t.descricao";
+        return super.read(JPQL);
+    }
+    public List<Tarifa> read(String filtro){
+        String JPQL="select t from Tarifa t "
+                + "where t.descricao like ?1 order by t.descricao";
+        return super.read(JPQL,"%"+filtro.toUpperCase()+"%");
+    }
+    public List<Tarifa> read(PontoTuristico pontoTuristico){
+        String JPQL="select t from Tarifa t where t.pontoTuristico=?1 order by t.descricao";
+        return super.read(JPQL, pontoTuristico);
+    }
+}
